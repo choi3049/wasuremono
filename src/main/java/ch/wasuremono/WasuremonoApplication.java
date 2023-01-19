2ff39734689a7cf268b7588405c53cdad70430cf
CHOE BYEONGSEON <qudtjs3012@naver.com>
2023-01-19 22:58:33 +0900

    Create project

---
+++ src/main/java/ch/wasuremono/WasuremonoApplication.java
@@ -0,0 +1,13 @@
+package ch.wasuremono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasuremonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasuremonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/wasuremono/WasuremonoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wasuremono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasuremonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
