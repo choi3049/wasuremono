f2b8569bb96e662406c06608598b89f1b01d7e7f
CHOE BYEONGSEON <qudtjs3012@naver.com>
2023-01-22 01:43:30 +0900

    Login page settings

--- src/main/java/ch/wasuremono/web/basic/homeController.java
+++ src/main/java/ch/wasuremono/web/basic/homeController.java
@@ -1,16 +1,22 @@
 package ch.wasuremono.web.basic;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("index")
 public class homeController {
 
-    @RequestMapping("/index")
+    @GetMapping
     public String home() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login() { return "login"; }
 }
